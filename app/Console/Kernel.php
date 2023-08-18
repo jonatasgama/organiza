11f3b0079f35bdd80a3c4b07c78a335bba9eaa8f
Jonatas Gama Souza <gamasouza.jonatas@gmail.com>
2023-08-18 08:32:33 -0300

    atualizando horario da cron para 9hs

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -12,7 +12,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        $schedule->command('emaillembrete:cron')->dailyAt('08:00');
+        $schedule->command('emaillembrete:cron')->dailyAt('09:00');
     }
 
     /**
