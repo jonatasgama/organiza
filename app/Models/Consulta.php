a0895a919a01a73f03877fd438fd56d203a7e4c5
Jonatas Gama Souza <gamasouza.jonatas@gmail.com>
2023-08-19 13:40:09 -0300

    add soft deletes na consulta

--- app/Models/Consulta.php
+++ app/Models/Consulta.php
@@ -4,12 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Consulta extends Model
 {
     use HasFactory;
+    use SoftDeletes;
 
     protected $fillable = ['paciente_id', 'tratamento_id', 'pagamento_id', 'inicio_consulta', 'fim_consulta', 'pagamento'];
     
