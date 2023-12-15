07d8f218d922b9cba9939bf5d8236392c0fdbe9a
Jonatas Gama Souza <gamasouza.jonatas@gmail.com>
2023-12-14 23:35:01 -0300

    ajuste no nome do app

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,7 +2,7 @@
 <tr>
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
-@if (trim($slot) === 'Mônica Vieira Psicologia')
+@if (trim($slot) === 'Monica Vieira Psicologia')
 <img src={{ config('app.name').'/img/logo.png' }} class="logo" alt="Monica Vieira Logo">
 @else
 {{ $slot }}
