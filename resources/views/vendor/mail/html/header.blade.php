c1c944538dcd805ba6aa022c5f2cba2c4986384b
Jonatas Gama Souza <gamasouza.jonatas@gmail.com>
2023-12-23 20:03:46 -0300

    ajustando href do email

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,7 +1,7 @@
 @props(['url'])
 <tr>
 <td class="header">
-<a href="{{ $url }}" style="display: inline-block;">
+<a href="psicologamonicavieira.com.br" style="display: inline-block;">
 @if (trim($slot) === 'Monica Vieira Psicologia')
 <img src='{{ env('APP_URL') }}/img/logo.png' class="logo" alt="Monica Vieira Logo">
 @else
